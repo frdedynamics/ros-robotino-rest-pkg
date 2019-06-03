8bc3e1b3774abd702f014485ad8d24d67aee2aab
Johannes <johannes.mogster@mojotech.no>
2019-06-03 18:56:46 +0200

    refactor

--- scripts/digitalinput.py
+++ scripts/digitalinput.py
@@ -4,12 +4,12 @@
 import sys
 import requests
 import rospy
-from robotino_rest_node.msg import DigitalReadings
+from ros_robotino_rest_pkg.msg import DigitalReadings
 
 
 # api-endpoint
 URL = "http://127.0.0.1/data/digitalinputarray"
-PARAMS = {'sid': 'robotino_rest_node'}
+PARAMS = {'sid': 'ros_robotino_rest_pkg'}
 
 
 def talker():
--- scripts/distancesensor.py
+++ scripts/distancesensor.py
@@ -4,12 +4,12 @@
 import sys
 import requests
 import rospy
-from robotino_rest_node.msg import AnalogReadings
+from ros_robotino_rest_pkg.msg import AnalogReadings
 
 
 # api-endpoint
 URL = "http://127.0.0.1/data/distancesensorarray"
-PARAMS = {'sid': 'robotino_rest_node'}
+PARAMS = {'sid': 'ros_robotino_rest_pkg'}
 
 
 def talker():
